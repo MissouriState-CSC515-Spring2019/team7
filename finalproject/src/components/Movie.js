82a46adac773714cbd9fcc16178df19238f34ec7
ScottPopken <sfp123@live.missouristate.edu>
2019-05-01 15:49:43 -0500

    o

---
+++ finalproject/src/components/Movie.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+const API_KEY = '36341ad74dd05db38d810686efc2d20f';
+
+class Movie extends Component {
+    state = {
+        activeMovie : []
+    }
+    componentDidMount = async () => {
+        const TITLE = this.props.location.state.movie
+        const API_CALL_SIGNLE = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${TITLE}`);
+        const movieData = await API_CALL_SIGNLE.json();
+        this.setState({
+            activeMovie: movieData.results[0]
+        });
+    }
+    
+    render(){
+        const movie = this.state.activeMovie;
+        return(
+            <div className="container">
+                <div className="active-movie">
+                    {movie.poster_path && <img className="acitve-moive__img" src={`http://image.tmdb.org/t/p/w185${movie.poster_path}`} alt={`${movie.title}`} />}
+                    {movie.poster_path && movie.title && <h3 className="active-movie__title">{movie.title}</h3>}
+                    {movie.poster_path && movie.release_date && <h4 className="active-movie__date">Release Date: {new Date(`${movie.release_date}`).toDateString()}</h4>}
+                    {movie.poster_path && movie.overview && <p className="movie_key">Overview: <span className="active-movie__overview">{movie.overview}</span></p>}
+                </div>
+            </div>
+        )
+    }
+}
+export default Movie
